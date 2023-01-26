9d48190e424ffbdaf54a88c61de1e51ff7673d4b
Bart Van Bos <bartvanbos@gmail.com>
2023-01-26 18:18:12 +0100

    added client/server certs for abc/def tier1 or ingress HTTPS termination

--- certs/certs.sh
+++ certs/certs.sh
@@ -20,6 +20,44 @@ if [[ $1 = "standby-cluster" ]]; then
   exit 0
 fi
 
+if [[ $1 = "app-abc-client" ]]; then
+  DOMAIN=abc.tetrate.prod
+  mkdir -p ./app-abc
+  openssl req -out ./app-abc/client.${DOMAIN}.csr -newkey rsa:4096 -sha512 -nodes -keyout ./app-abc/client.${DOMAIN}.key -subj "/CN=client.${DOMAIN}/O=Client"
+  openssl x509 -req -sha512 -days 3650 -CA ./root-cert.pem -CAkey ./root-key.pem -set_serial 1 -in ./app-abc/client.${DOMAIN}.csr -out ./app-abc/client.${DOMAIN}.pem
+  exit 0
+fi
+
+if [[ $1 = "app-abc-server" ]]; then
+  DOMAIN=abc.tetrate.prod
+  mkdir -p ./app-abc
+  openssl req -out ./app-abc/server.${DOMAIN}.csr -newkey rsa:4096 -sha512 -nodes -keyout ./app-abc/server.${DOMAIN}.key -subj "/CN=${DOMAIN}/O=Istio"
+  openssl x509 -req -sha512 -days 3650 -CA ./root-cert.pem -CAkey ./root-key.pem -set_serial 0 -in ./app-abc/server.${DOMAIN}.csr -out ./app-abc/server.${DOMAIN}.pem -extfile <(printf "subjectAltName=DNS:${DOMAIN},DNS:*.${DOMAIN},DNS:localhost")
+  cat ./app-abc/server.${DOMAIN}.pem ./root-cert.pem >> ./app-abc/server.${DOMAIN}-bundle.pem
+  exit 0
+fi
+
+if [[ $1 = "app-def-client" ]]; then
+  DOMAIN=def.tetrate.prod
+  mkdir -p ./app-def
+  openssl req -out ./app-def/client.${DOMAIN}.csr -newkey rsa:4096 -sha512 -nodes -keyout ./app-def/client.${DOMAIN}.key -subj "/CN=client.${DOMAIN}/O=Client"
+  openssl x509 -req -sha512 -days 3650 -CA ./root-cert.pem -CAkey ./root-key.pem -set_serial 1 -in ./app-def/client.${DOMAIN}.csr -out ./app-def/client.${DOMAIN}.pem
+  exit 0
+fi
+
+if [[ $1 = "app-def-server" ]]; then
+  DOMAIN=def.tetrate.prod
+  mkdir -p ./app-def
+  openssl req -out ./app-def/server.${DOMAIN}.csr -newkey rsa:4096 -sha512 -nodes -keyout ./app-def/server.${DOMAIN}.key -subj "/CN=${DOMAIN}/O=Istio"
+  openssl x509 -req -sha512 -days 3650 -CA ./root-cert.pem -CAkey ./root-key.pem -set_serial 0 -in ./app-def/server.${DOMAIN}.csr -out ./app-def/server.${DOMAIN}.pem -extfile <(printf "subjectAltName=DNS:${DOMAIN},DNS:*.${DOMAIN},DNS:localhost")
+  cat ./app-def/server.${DOMAIN}.pem ./root-cert.pem >> ./app-def/server.${DOMAIN}-bundle.pem
+  exit 0
+fi
+
+########################################
+### Print the generated certificates ###
+########################################
+
 if [[ $1 = "print-root-ca" ]]; then
   openssl x509 -in ./root-cert.pem -text
   exit 0
@@ -40,13 +78,45 @@ if [[ $1 = "print-standby-cluster" ]]; then
   exit 0
 fi
 
+if [[ $1 = "print-app-abc-client" ]]; then
+  DOMAIN=abc.tetrate.prod
+  openssl x509 -in ./app-abc/client.${DOMAIN}.pem -text
+  exit 0
+fi
+
+if [[ $1 = "print-app-abc-server" ]]; then
+  DOMAIN=abc.tetrate.prod
+  openssl x509 -in ./app-abc/server.${DOMAIN}.pem -text
+  exit 0
+fi
+
+if [[ $1 = "print-app-def-client" ]]; then
+  DOMAIN=def.tetrate.prod
+  openssl x509 -in ./app-def/client.${DOMAIN}.pem -text
+  exit 0
+fi
+
+if [[ $1 = "print-app-def-server" ]]; then
+  DOMAIN=def.tetrate.prod
+  openssl x509 -in ./app-def/server.${DOMAIN}.pem -text
+  exit 0
+fi
+
 echo "Please specify one of the following action:"
 echo "  - root-ca"
 echo "  - mgmt-cluster"
 echo "  - active-cluster"
 echo "  - standby-cluster"
+echo "  - app-abc-client"
+echo "  - app-abc-server"
+echo "  - app-def-client"
+echo "  - app-def-server"
 echo "  - print-root-ca"
 echo "  - print-mgmt-cluster"
 echo "  - print-active-cluster"
 echo "  - print-standby-cluster"
+echo "  - print-app-abc-client"
+echo "  - print-app-abc-server"
+echo "  - print-app-def-client"
+echo "  - print-app-def-server"
 exit 1
