8168b1a9028db07ad215bc65cf0d083eeb270923
Bart Van Bos <bart@tetrate.io>
2023-03-12 20:41:30 +0100

    wip

--- scenario.sh
+++ scenario.sh
@@ -2,17 +2,22 @@
 ROOT_DIR="$( cd -- "$(dirname "${0}")" >/dev/null 2>&1 ; pwd -P )"
 source ${ROOT_DIR}/env.sh ${ROOT_DIR}
 source ${ROOT_DIR}/helpers.sh
+SCENARIO=$(get_scenario)
 SCENARIO_DIR=$(get_scenario_dir)
 
 ACTION=${1}
 
 if [[ ${ACTION} = "deploy" ]]; then
+  print_info "Start deploying scenario ${SCENARIO}"
   ${SCENARIO_DIR}/run.sh ${ROOT_DIR} deploy
+  print_info "Finished deploying scenario ${SCENARIO}"
   exit 0
 fi
 
 if [[ ${ACTION} = "undeploy" ]]; then
+  print_info "Start undeploying scenario ${SCENARIO}"
   ${SCENARIO_DIR}/run.sh ${ROOT_DIR} undeploy
+  print_info "Finished undeploying scenario ${SCENARIO}"
   exit 0
 fi
 
