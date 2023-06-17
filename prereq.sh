fc8d494bc57e5c8b4c7cdd7f4476bcc651b4743d
Bart Van Bos <bart@tetrate.io>
2023-06-17 06:28:40 +0200

    remove argocd as a mandatory prerequisite

--- prereq.sh
+++ prereq.sh
@@ -15,7 +15,8 @@ TSB_REPO_PW=$(get_tetrate_repo_password) ;
 
 if [[ ${ACTION} = "check" ]]; then
 
-  DEPENDENCIES=( tctl minikube kind k3d expect docker kubectl jq awk curl nc argocd )
+  # TODO: DEPENDENCIES=( tctl minikube kind k3d expect docker kubectl jq awk curl nc argocd )
+  DEPENDENCIES=( tctl minikube kind k3d expect docker kubectl jq awk curl nc )
 
   # check necessary dependencies are installed
   echo "Checking if all software dependencies installed : ok"
