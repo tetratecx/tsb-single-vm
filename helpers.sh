0ee6ed4d6c3c5004b9943c299be4ed95b0110901
Bart Van Bos <bart@tetrate.io>
2023-11-18 08:49:37 +0100

    clean-up multipass.sh

--- helpers.sh
+++ helpers.sh
@@ -1,40 +1,105 @@
 #!/usr/bin/env bash
+#
+# Helper script with colorful print functions
 
-# Colors
-end="\033[0m"
-black="\033[0;30m"
-blackb="\033[1;30m"
-white="\033[0;37m"
-whiteb="\033[1;37m"
-red="\033[0;31m"
-redb="\033[1;31m"
-green="\033[0;32m"
-greenb="\033[1;32m"
-yellow="\033[0;33m"
-yellowb="\033[1;33m"
-blue="\033[0;34m"
-blueb="\033[1;34m"
-purple="\033[0;35m"
-purpleb="\033[1;35m"
-lightblue="\033[0;36m"
-lightblueb="\033[1;36m"
+# Color values sorted as
+#   - bold (<NAME>_B)
+#   - marker-background (<NAME>_M)
+#   - underline (<NAME>_U)
+#   - normal (<NAME>)
+#
+# END is the terminator to skip back to default color
 
-# Print info messages
+END="\033[0m"
+
+{
+  BLACK_B="\033[1;30m"
+  BLACK_M="\033[40m"
+  BLACK_U="\033[4;30m"
+  BLACK="\033[0;30m"
+
+  BLUE_B="\033[1;34m"
+  BLUE_M="\033[44m"
+  BLUE_U="\033[4;34m"
+  BLUE="\033[0;34m"
+
+  GRAY_B="\033[1;90m"
+  GRAY_M="\033[100m"
+  GRAY_U="\033[4;90m"
+  GRAY="\033[0;90m"
+
+  GREEN_B="\033[1;32m"
+  GREEN_M="\033[42m"
+  GREEN_U="\033[4;32m"
+  GREEN="\033[0;32m"
+
+  LIGHTBLUE_B="\033[1;36m"
+  LIGHTBLUE_M="\033[46m"
+  LIGHTBLUE_U="\033[4;36m"
+  LIGHTBLUE="\033[0;36m"
+
+  LIGHTGRAY_B="\033[1;97m"
+  LIGHTGRAY_M="\033[107m"
+  LIGHTGRAY_U="\033[4;97m"
+  LIGHTGRAY="\033[0;97m"
+
+  LIGHTGREEN_B="\033[1;92m"
+  LIGHTGREEN_M="\033[102m"
+  LIGHTGREEN_U="\033[4;92m"
+  LIGHTGREEN="\033[0;92m"
+
+  LIGHTPURPLE_B="\033[1;94m"
+  LIGHTPURPLE_M="\033[104m"
+  LIGHTPURPLE_U="\033[4;94m"
+  LIGHTPURPLE="\033[0;94m"
+
+  LIGHTRED_B="\033[1;91m"
+  LIGHTRED_M="\033[101m"
+  LIGHTRED_U="\033[4;91m"
+  LIGHTRED="\033[0;91m"
+
+  LIGHTYELLOW_B="\033[1;93m"
+  LIGHTYELLOW_M="\033[103m"
+  LIGHTYELLOW_U="\033[4;93m"
+  LIGHTYELLOW="\033[0;93m"
+
+  PURPLE_B="\033[1;35m"
+  PURPLE_M="\033[45m"
+  PURPLE_U="\033[4;35m"
+  PURPLE="\033[0;35m"
+
+  RED_B="\033[1;31m"
+  RED_M="\033[41m"
+  RED_U="\033[4;31m"
+  RED="\033[0;31m"
+
+  WHITE_B="\033[1;37m"
+  WHITE_M="\033[47m"
+  WHITE_U="\033[4;37m"
+  WHITE="\033[0;37m"
+
+  YELLOW_B="\033[1;33m"
+  YELLOW_M="\033[43m"
+  YELLOW_U="\033[4;33m"
+  YELLOW="\033[0;33m"
+}
+
 function print_info {
-  echo -e "${greenb}${1}${end}"
+  echo -e "${GREEN_B}${1}${END}"
 }
 
-# Print warning messages
 function print_warning {
-  echo -e "${yellowb}${1}${end}"
+  echo -e "${YELLOW_B}${1}${END}"
 }
 
-# Print error messages
 function print_error {
-  echo -e "${redb}${1}${end}"
+  echo -e "${RED_B}${1}${END}"
 }
 
-# Print command messages
 function print_command {
-  echo -e "${lightblueb}${1}${end}"
+  echo -e "${LIGHTBLUE_B}${1}${END}"
+}
+
+function print_stage {
+  echo -e "${BLUE_B}${1}${END}"
 }
--- multipass.sh
+++ multipass.sh
@@ -1,32 +1,125 @@
 #!/usr/bin/env bash
 #
-# Helper script to create a local Ubuntu VM for MacOS or Windows
+# Helper script to manage a local Multipass Ubuntu VM for MacOS or Windows
 # because the docker network stack on those OS's is not useable.
 #
 
-ROOT_DIR="$( cd -- "$(dirname "${0}")" >/dev/null 2>&1 ; pwd -P )"
+BASE_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+export BASE_DIR
+source "${BASE_DIR}/helpers.sh"
 
-source ${ROOT_DIR}/helpers.sh
+ACTION=${1}
 
-CPU_MASTER=2
-MEM_MASTER=16G
-DISK_MASTER=20G
-RUN_OS=22.04
-VM_NAME=tsb-single-vm
+VM_CPU="${VM_CPU:-2}"
+VM_DISK="${VM_DISK:-20G}"
+VM_MEM="${VM_MEM:-16G}"
+VM_OS="${VM_OS:-22.04}"
+VM_NAME="${VM_NAME:-tsb-single-vm}"
 
-print_info "Creating ${VM_NAME} VM" ;
-multipass launch --cpus "${CPU_MASTER}" \
-                 --disk "${DISK_MASTER}" \
-                 --memory "${MEM_MASTER}" \
-                 --mount "${PWD}:/home/ubuntu/tsb-single-vm" \
-                 --name "${VM_NAME}" \
-                 "${RUN_OS}" ;
+# This function provides help information for the script.
+#
+function help() {
+  echo "Usage: $0 <command> [options]"
+  echo "Commands:"
+  echo "  --start     Start multipass Ubuntu VM."
+  echo "  --shell     Spawn a shell into multipass Ubuntu VM."
+  echo "  --stop      Stop multipass Ubuntu VM."
+  echo "  --delete    Delete multipass Ubuntu VM."
+  echo "  --recreate  Recreate multipass Ubuntu VM."
+  echo "  --help      Display this help message."
+}
 
-print_info "Installing packages in VM ${VM_NAME}" ;
-multipass exec "${VM_NAME}" -- bash -c "sudo apt-get -y update && sudo apt-get install -y docker.io net-tools expect curl jq" ;
-multipass exec "${VM_NAME}" -- bash -c "sudo usermod -aG docker ubuntu" ;
+# This function starts multipass Ubuntu VM.
+#
+function start() {
 
-print_info "Starting shell in VM ${VM_NAME}" ;
-print_command "multipass shell ${VM_NAME}" ;
-multipass shell ${VM_NAME} ;
+  if multipass list --format json | jq -r '.list[].name' 2>/dev/null | grep -q "${VM_NAME}" ; then
+    echo "VM ${VM_NAME} already exists" ;
+    if multipass list --format json | jq -r '.list[].state' 2>/dev/null | grep -q "Running" ; then
+      echo "VM ${VM_NAME} is already running" ;
+    elif multipass list --format json | jq -r '.list[].state' 2>/dev/null | grep -q "Stopped" ; then
+      echo "Start ${VM_NAME} VM" ;
+      multipass start "${VM_NAME}" ;
+    else
+      echo "VM ${VM_NAME} is in an unknown state" ;
+    fi
+  else
+    echo "Create ${VM_NAME} VM" ;
+    multipass launch --cpus "${VM_CPU}" \
+                    --disk "${VM_DISK}" \
+                    --memory "${VM_MEM}" \
+                    --mount "${PWD}:/home/ubuntu/tsb-single-vm" \
+                    --name "${VM_NAME}" \
+                    "${VM_OS}" ;
 
+    echo "Install packages in VM ${VM_NAME}" ;
+    multipass exec "${VM_NAME}" -- bash -c "sudo apt-get -y update && sudo apt-get install -y curl docker.io expect jq make net-tools" ;
+    multipass exec "${VM_NAME}" -- bash -c "sudo usermod -aG docker ubuntu" ;
+  fi
+
+}
+
+# This function stops multipass Ubuntu VM.
+#
+function stop() {
+
+  echo "Stop ${VM_NAME} VM" ;
+  if multipass list --format json | jq -r '.list[].name' 2>/dev/null | grep -q "${VM_NAME}" ; then
+    multipass stop "${VM_NAME}" ;
+  fi
+
+}
+
+# This function launches a shell in multipass Ubuntu VM.
+#
+function shell() {
+
+  echo "Spawn a shell in VM ${VM_NAME}" ;
+  multipass shell "${VM_NAME}" ;
+
+}
+
+# This function deletes multipass Ubuntu VM.
+#
+function delete() {
+
+  echo "Delete VM ${VM_NAME}" ;
+  if multipass list --format json | jq -r '.list[].name' 2>/dev/null | grep -q "${VM_NAME}" ; then
+    multipass delete --purge "${VM_NAME}" ;
+  fi  
+
+}
+
+
+# Main execution
+#
+case "${ACTION}" in
+  --help)
+    help ;
+    ;;
+  --start)
+    print_stage "Going start the multipass Ubuntu VM" ;
+    start ;
+    ;;
+  --stop)
+    print_stage "Going start the multipass Ubuntu VM" ;
+    stop ;
+    ;;
+  --shell)
+    print_stage "Going spawn a shell into the multipass Ubuntu VM" ;
+    shell ;
+    ;;
+  --delete)
+    print_stage "Going to delete the multipass Ubuntu VM" ;
+    delete ;
+    ;;
+  --recreate)
+    print_stage "Going to recreate the multipass Ubuntu VM" ;
+    delete ;
+    start ;
+    ;;
+  *)
+    print_error "Invalid option. Use 'help' to see available commands." ;
+    help ;
+    ;;
+esac
