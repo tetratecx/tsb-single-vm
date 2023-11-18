59d5ef024474252b7d2f0840842c03126982bde5
Bart Van Bos <bart@tetrate.io>
2023-11-18 01:33:52 +0100

    added packages to multipass VM

--- multipass.sh
+++ multipass.sh
@@ -14,15 +14,19 @@ DISK_MASTER=20G
 RUN_OS=22.04
 VM_NAME=tsb-single-vm
 
-print_info "Creating ${VM_NAME} VM"
-multipass launch \
-  --cpus "${CPU_MASTER}" \
-  --disk "${DISK_MASTER}" \
-  --memory "${MEM_MASTER}" \
-  --mount .:~/tsb-single-vm \
-  --name "${VM_NAME}" 
-  "${RUN_OS}" ;
+print_info "Creating ${VM_NAME} VM" ;
+multipass launch --cpus "${CPU_MASTER}" \
+                 --disk "${DISK_MASTER}" \
+                 --memory "${MEM_MASTER}" \
+                 --mount "${PWD}:/home/ubuntu/tsb-single-vm" \
+                 --name "${VM_NAME}" \
+                 "${RUN_OS}" ;
 
-print_info "Starting shell in VM ${VM_NAME}"
-print_command "multipass shell ${VM_NAME}"
+print_info "Installing packages in VM ${VM_NAME}" ;
+multipass exec "${VM_NAME}" -- bash -c "sudo apt-get -y update && sudo apt-get install -y docker.io net-tools expect curl jq" ;
+multipass exec "${VM_NAME}" -- bash -c "sudo usermod -aG docker ubuntu" ;
+
+print_info "Starting shell in VM ${VM_NAME}" ;
+print_command "multipass shell ${VM_NAME}" ;
 multipass shell ${VM_NAME} ;
+
