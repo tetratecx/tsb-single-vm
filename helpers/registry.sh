822570bd22e7afa87867e00cbdb1f1c1dc339050
Bart Van Bos <bart@tetrate.io>
2023-11-27 00:01:55 +0100

    improve helpers/registry.sh based on spellchecker suggestions

--- helpers/registry.sh
+++ helpers/registry.sh
@@ -3,6 +3,8 @@
 # Helper script to manage local docker registry and tsb images.
 #
 HELPERS_DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")") ;
+
+# shellcheck source=/dev/null
 source "${HELPERS_DIR}/print.sh" ;
 
 LOCAL_REGISTRY_NAME="${LOCAL_REGISTRY_NAME:-registry}"
@@ -21,10 +23,10 @@ function start_local_registry {
   [[ -z "${2}" ]] && local network_name="${LOCAL_REGISTRY_NETWORK}" || local network_name="${2}" ;
   [[ -z "${3}" ]] && local network_subnet="${LOCAL_REGISTRY_SUBNET}" || local network_subnet="${3}" ;
 
-  if $(docker network ls --format "{{.Name}}" | grep "${network_name}" &>/dev/null); then
+  if docker network ls --format "{{.Name}}" | grep "${network_name}" &>/dev/null; then
     echo "Found docker network '${network_name}'" ;
   else
-    local network_gateway=$(echo ${network_subnet} |  awk -F '.' "{ print \$1\".\"\$2\".\"\$3\".\"1;}") ;
+    local network_gateway; network_gateway=$(echo "${network_subnet}" |  awk -F '.' "{ print \$1\".\"\$2\".\"\$3\".\"1;}") ;
     echo "Starting docker network '${network_name}' (subnet: '${network_subnet}', gateway: '${network_gateway}')" ;
     docker network create \
       --driver="bridge" \
@@ -48,10 +50,10 @@ function start_local_registry {
     *)
       echo "Start container '${container_name}' in docker network '${network_name}'" ;
       docker run --detach \
-        --hostname ${container_name} \
-        --name ${container_name} \
-        --network ${network_name} \
-        --publish ${LOCAL_REGISTRY_PORT}:5000 \
+        --hostname "${container_name}" \
+        --name "${container_name}" \
+        --network "${network_name}" \
+        --publish "${LOCAL_REGISTRY_PORT}:5000" \
         --restart always \
         registry:2 ;
         ;;
@@ -63,7 +65,7 @@ function start_local_registry {
 #     (1) container name (optional, default 'registry')
 function stop_local_registry {
   [[ -z "${1}" ]] && local container_name="${LOCAL_REGISTRY_NAME}" || local container_name="${1}" ;
-  docker stop ${container_name} 2>/dev/null ;
+  docker stop "${container_name}" 2>/dev/null ;
   print_info "Local docker registry '${container_name}' stopped"
 }
 
@@ -75,9 +77,9 @@ function remove_local_registry {
   [[ -z "${1}" ]] && local container_name="${LOCAL_REGISTRY_NAME}" || local container_name="${1}" ;
   [[ -z "${2}" ]] && local network_name="${LOCAL_REGISTRY_NETWORK}" || local network_name="${2}" ;
 
-  docker stop ${container_name} 2>/dev/null ;
-  docker rm --volumes ${container_name} 2>/dev/null ;
-  docker network rm ${network_name} 2>/dev/null ;
+  docker stop "${container_name}" 2>/dev/null ;
+  docker rm --volumes "${container_name}" 2>/dev/null ;
+  docker network rm "${network_name}" 2>/dev/null ;
   print_info "Local docker registry '${container_name}' removed" ;
 }
 
@@ -87,7 +89,7 @@ function remove_local_registry {
 function get_local_registry_endpoint {
   [[ -z "${1}" ]] && local container_name="${LOCAL_REGISTRY_NAME}" || local container_name="${1}" ;
 
-  local registry_ip=$(docker container inspect --format '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "${container_name}" 2>/dev/null | awk NF) ;
+  local registry_ip; registry_ip=$(docker container inspect --format '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "${container_name}" 2>/dev/null | awk NF) ;
   if [[ -z "${registry_ip}" ]]; then
     print_error "Container '${container_name}' has no ip address or is not running" ;
     return 1 ;
@@ -101,7 +103,7 @@ function get_local_registry_endpoint {
 function add_insecure_registry {
   [[ -z "${1}" ]] && local container_name="${LOCAL_REGISTRY_NAME}" || local container_name="${1}" ;
 
-  local registry_endpoint=$(get_local_registry_endpoint "${container_name}") ;
+  local registry_endpoint; registry_endpoint=$(get_local_registry_endpoint "${container_name}") ;
   local docker_json="{\"insecure-registries\" : [\"http://${registry_endpoint}\"]}" ;
 
   # In case no local docker configuration file yet, create new from scratch
@@ -109,7 +111,7 @@ function add_insecure_registry {
     sudo sh -c "echo '${docker_json}' > /etc/docker/daemon.json" ;
     sudo systemctl restart docker ;
     print_info "Insecure registry configured" ;
-  elif cat /etc/docker/daemon.json | grep ${registry_endpoint} &>/dev/null; then
+  elif grep -q "${registry_endpoint}" /etc/docker/daemon.json; then
     print_info "Insecure registry already configured" ;
   else
     print_warning "File /etc/docker/daemon.json already exists" ;
@@ -126,18 +128,18 @@ function sync_single_image {
   [[ -z "${1}" ]] && print_error "Please provide local registry as 1st argument" && return 2 || local local_registry="${1}" ;
   [[ -z "${2}" ]] && print_error "Please provide image as 2nd argument" && return 2 || local image="${2}" ;
 
-  local image_without_registry=$(echo ${image} | sed "s|containers.dl.tetrate.io/||") ;
-  local image_name=$(echo ${image_without_registry} | awk -F: '{print $1}') ;
-  local image_tag=$(echo ${image_without_registry} | awk -F: '{print $2}') ;
+  local image_without_registry; image_without_registry=$(echo "${image}" | awk -F/ '{print $NF}') ;
+  local image_name; image_name=$(echo "${image_without_registry}" | awk -F: '{print $1}') ;
+  local image_tag; image_tag=$(echo "${image_without_registry}" | awk -F: '{print $2}') ;
 
-  if ! docker image inspect ${image} &>/dev/null ; then
-    docker pull ${image} ;
+  if ! docker image inspect "${image}" &>/dev/null ; then
+    docker pull "${image}" ;
   fi
-  if ! docker image inspect ${local_registry}/${image_without_registry} &>/dev/null ; then
-    docker tag ${image} ${local_registry}/${image_without_registry} ;
+  if ! docker image inspect "${local_registry}/${image_without_registry}" &>/dev/null ; then
+    docker tag "${image} ${local_registry}/${image_without_registry}" ;
   fi
-  if ! curl -s -X GET ${local_registry}/v2/${image_name}/tags/list | grep "${image_tag}" &>/dev/null ; then
-    docker push ${local_registry}/${image_without_registry} ;
+  if ! curl -s -X GET "${local_registry}/v2/${image_name}/tags/list" | grep "${image_tag}" &>/dev/null ; then
+    docker push "${local_registry}/${image_without_registry}" ;
   fi
 
   echo "Image '${image}' synced to '${local_registry}'" ;
@@ -165,7 +167,7 @@ function sync_tsb_images {
   fi
 
   # Sync all tsb images to local repo
-  for image in `tctl install image-sync --just-print --raw --accept-eula 2>/dev/null` ; do
+  for image in $(timeout 3s tctl install image-sync --just-print --accept-eula 2>/dev/null) ; do
     sync_single_image "${local_registry}" "${image}" ;
   done
 
