a7e0a7e7db4ad6a27d8d99baff0340eac75c1f9d
RicHincapie <hincapiet.ricardo@gmail.com>
2024-10-26 11:42:17 +1000

    add func upgrade_mp_with_tctl and sync_tsb_images

--- upgrade.sh
+++ upgrade.sh
@@ -6,13 +6,17 @@ BASE_DIR="$( cd -- "$(dirname "${0}")" >/dev/null 2>&1 ; pwd -P )" ;
 source "${BASE_DIR}/env.sh" ;
 # shellcheck source=/dev/null
 source "${BASE_DIR}/helpers/print.sh" ;
+# shellcheck source=/dev/null
+source "${BASE_DIR}/helpers/registry.sh" ;
 
 ACTION=${1} ;
 
 TSB_HELM_REPO="https://charts.dl.tetrate.io/public/helm/charts" ;
 MP_HELM_CHART="tetrate-tsb-charts/managementplane" ;
 CP_HELM_CHART="tetrate-tsb-charts/controlplane" ;
-
+BASE_DIR_UPGRADE="${BASE_DIR}/upgrade"
+CLUSTERS=( c1 c2 )
+MP="t1"
 
 # This function provides help information for the script.
 #
@@ -24,16 +28,14 @@ function help() {
 # This function backs up install manifests
 #
 function backup_manifests() {
-  local clusters=( c1 c2 )
-  local mp="t1"
-  local backup_dir="${BASE_DIR}/upgrade/backups"
+  local backup_dir="${BASE_DIR_UPGRADE}/backups"
   mkdir -p "${backup_dir}"
-  print_info "Creating backups for clusters ${clusters} and mp ${mp} at ${backup_dir}" ;
-  for cluster in "${clusters[@]}"; do
+  print_info "Creating backups for clusters ${CLUSTERS} and mp ${MP} at ${backup_dir}" ;
+  for cluster in "${CLUSTERS[@]}"; do
     #wait_cluster_onboarded "${cluster}" ;
-    kubectl --context "${cluster}" get controlplane controlplane -n istio-system -oyaml > "${backup_dir}/${cluster}-controlplane-backup.yaml" ;
+    kubectl --context "${cluster}" get controlplane -n istio-system -oyaml > "${backup_dir}/${cluster}-controlplane-backup.yaml" ;
   done
-  kubectl --context "${mp}" get controlplane controlplane -n istio-system -oyaml > "${backup_dir}/${mp}-managementplane-backup.yaml" ;
+  kubectl --context "${MP}" get managementplane -n tsb -oyaml > "${backup_dir}/${mp}-managementplane-backup.yaml" ;
 }
 
 # This function gets local version in format 1.9.3
@@ -74,8 +76,36 @@ function upgrade_tctl() {
 
 
 
+function upgrade_mp_with_tctl {
+  local local_registry="$(get_local_registry_endpoint)"
+  local output_dir="${BASE_DIR_UPGRADE}/mp"
+
+  tctl install manifest management-plane-operator \
+      --registry "${local_registry}" \
+      > "${output_dir}/managementplaneoperator.yaml"
+
+  if tctl; then
+    if [[ -s "${output_dir}/managementplaneoperator.yaml" ]]
+      kubectl apply --context "${MP}" -f "${output_dir}/managementplaneoperator.yaml"
+      print_info "Waiting for new CRD managementoperator to be processed"
+      wait 60
+      kubectl get pod -n tsb --context "${MP}"
+    else
+      print_error "${output_dir}/managementplaneoperator.yaml is empty"
+      exit 1
+    fi
+  else
+    print_error "tctl install manifest management-plane-operator failed"
+    exit 1
+  fi
+
+}
+
+
 # Main execution
 #
 
-# backup_manifests
-# upgrade_tctl
+# backup_manifests ;
+# upgrade_tctl ;
+# sync_tsb_images "$(get_local_registry_endpoint)" "$(get_tetrate_repo_user)" "$(get_tetrate_repo_password)" ;
+# upgrade_mp_with_tctl
